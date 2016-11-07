39408250ceaea3323470d47a857a29d8f3f32e01
Christoffer Palm <christoffer.palm@oddhill.se>
2016-11-07 16:49:35 +0100

    Updated the import controller to use the actual plugin definitions

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -7,7 +7,9 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\Url;
 use Drupal\import_api\ImporterManager;
 use Drupal\import_api\ImporterService;
+use Drupal\import_api\Plugin\ImporterPluginBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ImportController implements ContainerInjectionInterface {
 
@@ -51,48 +53,66 @@ class ImportController implements ContainerInjectionInterface {
    * @return array
    */
   public function listImporters() {
-    //$plugin_definitions = $this->importerManager->getDefinitions();
+    $plugin_definitions = $this->importerManager->getDefinitions();
+
+    $header = [
+      'importer' => new TranslatableMarkup('Importer'),
+      'imported_every' => new TranslatableMarkup('Imported every'),
+      'last_import' => new TranslatableMarkup('Last import'),
+      'actions' => new TranslatableMarkup('Actions'),
+    ];
 
     $output = [
       '#type' => 'table',
-      '#header' => ['Importer', 'Imported every', 'Last import', 'Actions'],
+      '#header' => $header,
       '#empty' => new TranslatableMarkup('There are no importers.'),
     ];
 
-    $output[] = [
-      'title' => [
-        '#markup' => 'Testing',
-      ],
-      'imported_every' => [
-        '#markup' => '10',
-      ],
-      'last_import' => [
-        '#markup' => 'Today at 13:37',
-      ],
-      'operations' => [
-        '#type' => 'operations',
-        '#links' => [
-          'import' => [
-            'title' => 'Import',
-            'url' => Url::fromRoute('import_api.admin_config_importers_import'),
+    foreach ($plugin_definitions as $plugin_id => $definition) {
+      $output[$plugin_id] = [
+        'title' => [
+          '#markup' => $definition['label'],
+        ],
+        'imported_every' => [
+          '#markup' => 'N/A',
+        ],
+        'last_import' => [
+          '#markup' => 'N/A',
+        ],
+        'operations' => [
+          '#type' => 'operations',
+          '#links' => [
+            'import' => [
+              'title' => new TranslatableMarkup('Import'),
+              'url' => Url::fromRoute('import_api.admin_config_importers_import', [
+                'plugin_id' => $plugin_id,
+              ]),
+            ],
           ],
         ],
-      ],
-    ];
+      ];
+    }
 
     return $output;
   }
 
-  public function triggerImporter() {
-    batch_set([
-      'title' => new TranslatableMarkup('Testing batch'),
-      'operations' => [
-        ['import_api_batch_1', ['a string', ['foo' => 'bar']]],
-      ],
-      'finished' => 'import_api_batch_finished',
-      'file' => drupal_get_path('module', 'import_api') . '/import_api.module',
-    ]);
+  /**
+   * Starts a batch process for the specified importer plugin.
+   *
+   * @param $plugin_id
+   * @return null|RedirectResponse
+   */
+  public function triggerImporter($plugin_id) {
+    $redirect_url = Url::fromRoute('import_api.admin_config_importers');
 
-    return batch_process(Url::fromRoute('import_api.admin_config_importers'));
+    /** @var ImporterPluginBase $importer */
+    $importer = $this->importerManager
+      ->createInstance($plugin_id);
+
+    $this->importerService
+      ->createBatchFor($importer);
+
+    return $this->importerService
+      ->batchProcess($redirect_url);
   }
 }
